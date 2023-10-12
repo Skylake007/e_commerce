222f3b6e6535732157d754762ce702d4785e9651
Nguyen Thanh Binh <thanhbinhdev07@gmail.com>
2023-10-12 17:34:35 +0700

    +add Favorite list

--- lib/pages/my_cart_page.dart
+++ lib/pages/my_cart_page.dart
@@ -2,6 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:e_commerce/dummy/my_card_json.dart';
 import 'package:e_commerce/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dash/flutter_dash.dart';
 import 'package:line_icons/line_icons.dart';
 
 class MyCartPage extends StatefulWidget {
@@ -21,6 +22,7 @@ class _MyCartPageState extends State<MyCartPage> {
         child: getAppBar(),
       ),
       body: getBody(),
+      bottomNavigationBar: getBottom(),
     );
   }
 
@@ -84,6 +86,10 @@ class _MyCartPageState extends State<MyCartPage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           getCartLists(),
+          const SizedBox(height: 20),
+          getPromotionCode(),
+          const SizedBox(height: 30),
+          getSummary(),
         ],
       ),
     );
@@ -92,83 +98,261 @@ class _MyCartPageState extends State<MyCartPage> {
   Widget getCartLists() {
     var size = MediaQuery.of(context).size;
     return Column(
-      children: List.generate(myCartJson.length, (index) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 25),
-          child: FadeIn(
-            child: Row(
-              children: [
-                SizedBox(
-                  width: (size.width - 30) * 0.7,
-                  height: 80,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 70,
-                        height: 70,
-                        child: Stack(
-                          children: [
-                            Container(
+        children: List.generate(myCartJson.length, (index) {
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 25),
+        child: FadeIn(
+          child: Row(
+            children: [
+              SizedBox(
+                width: (size.width - 30) * 0.7,
+                height: 80,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 70,
+                      height: 70,
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage(myCartJson[index]['image']),
+                                    fit: BoxFit.cover)),
+                          ),
+                          Positioned(
+                            top: 10,
+                            child: Container(
                               width: 60,
                               height: 60,
                               decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: AssetImage(
-                                          myCartJson[index]['image']),
-                                      fit: BoxFit.cover)),
-                            ),
-                            Positioned(
-                              top: 10,
-                              child: Container(
-                                width: 60,
-                                height: 60,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(5),
-                                  border: Border.all(
-                                      color: secondary.withOpacity(0.2)),
-                                  color: secondary.withOpacity(0.1),
-                                ),
+                                borderRadius: BorderRadius.circular(5),
+                                border: Border.all(
+                                    color: secondary.withOpacity(0.2)),
+                                color: secondary.withOpacity(0.1),
                               ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          myCartJson[index]['name'],
+                          style: const TextStyle(fontSize: 16),
+                          maxLines: 2,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              "\$",
+                              style: TextStyle(
+                                  color: red,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              myCartJson[index]['price'],
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
                             ),
                           ],
                         ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 15),
+              const Flexible(
+                child: SizedBox(
+                  height: 80,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Icon(Icons.close, size: 15),
                       ),
-                      const SizedBox(width: 15),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(LineIcons.minusSquare),
+                          SizedBox(width: 10),
+                          Text(
+                            "1",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(LineIcons.plusSquare),
+                        ],
+                      ),
                     ],
                   ),
                 ),
-                const SizedBox(width: 10),
-                Flexible(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(myCartJson[index]['name'],
-                          style: const TextStyle(fontSize: 16), maxLines: 2),
-                      Row(
-                        children: [
-                          const Text(
-                            "\$",
-                            style: TextStyle(
-                                color: red,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            myCartJson[index]['price'],
-                            style: const TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              ],
+              )
+            ],
+          ),
+        ),
+      );
+    }));
+  }
+
+  Widget getPromotionCode() {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      height: 55,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: secondary.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: secondary.withOpacity(0.3),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: (size.width - 30) * 0.7,
+            height: 55,
+            child: const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                cursorColor: secondary,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Mã giảm giá",
+                ),
+              ),
             ),
           ),
-        );
-      }),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: secondary.withOpacity(0.3),
+                    border: Border.all(color: black),
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Center(child: Text("Chọn mã")),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getSummary() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            const Text(
+              "Tổng tiền hàng",
+              style: TextStyle(
+                  fontSize: 16, color: secondary, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 5),
+            const Text(
+              "(3sp)",
+              style: TextStyle(color: secondary, fontSize: 16),
+            ),
+            Dash(
+              length: 155,
+              direction: Axis.horizontal,
+              dashLength: 10,
+              dashColor: secondary.withOpacity(0.5),
+            ),
+            const SizedBox(width: 5),
+            const Text(
+              "\$600",
+              style: TextStyle(
+                  color: secondary, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+        Row(
+          children: [
+            const Text(
+              "Phí vận chuyển",
+              style: TextStyle(
+                  fontSize: 16, color: secondary, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 5),
+            Dash(
+              length: 160,
+              direction: Axis.horizontal,
+              dashLength: 10,
+              dashColor: secondary.withOpacity(0.5),
+            ),
+            const SizedBox(width: 5),
+            const Text(
+              "Miễn phí",
+              style: TextStyle(
+                  color: secondary, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+        Row(
+          children: [
+            const Text(
+              "Tổng ",
+              style: TextStyle(
+                  fontSize: 16, color: secondary, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 5),
+            Dash(
+              length: 260,
+              direction: Axis.horizontal,
+              dashLength: 10,
+              dashColor: secondary.withOpacity(0.5),
+            ),
+            const SizedBox(width: 5),
+            const Text(
+              "\$600",
+              style: TextStyle(
+                  color: secondary, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+      ],
+    );
+  }
+
+  Widget getBottom() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      child: Container(
+        height: 55,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Center(
+          child: Text(
+            "Thanh toán ngay",
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+              color: white,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
